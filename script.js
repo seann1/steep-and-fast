5d0cbcc181407c44ab36f4d81d8db704ee960a01
Sean Niesen <seanniesen@gmail.com>
2026-01-16 12:25:17 -0800

    expand width of shapes and add xVertCalc function

--- script.js
+++ script.js
@@ -5,45 +5,65 @@ const sketch = (p) => {
         p.createCanvas(p.windowWidth, p.windowHeight);
         p.background(220);
     };
+
+    let tx = 0;
+    let ty = 0;
+
 	function getDistanceFromMouse(x,y) {
-		const d = p.dist(x, y, p.mouseX, p.mouseY);
-		const max = p.dist(0, 0, 500, 500); // canvas diagonal
+        const globalX = x + tx;
+        const globalY = y + ty;
+
+		const d = p.dist(globalX, globalY, p.mouseX, p.mouseY) * 5;
+		const max = p.dist(0, 0, p.windowWidth, p.windowHeight); // canvas diagonal
+
 		return p.constrain(d / max, 0, 1);
 	}
+
+    function xVertCalc(x, y, i, operator) {
+        return operator === "-" ? x + (i*50) - (getDistanceFromMouse(x + (i*50), y)*50) :
+            x + (i*50) + (getDistanceFromMouse(x + (i*50), y)*50);
+    }
+
+    const lowX = -50;
+    const middleX = 0;
+    const highX = 50;
     p.draw = () => {
 		p.background(220);
+
+        tx = p.width / 2 - 250;
+        ty = p.height / 2 - 250;
+
 		p.push();
-        p.translate(p.width/2-250, p.height/2-250);
+        p.translate(tx, ty);
         p.fill(0, 0, 0);
         p.noStroke();
 
         for (let i = 0; i < 10; i++) {
-			const lerpColorResult = p.lerpColor(p.color(255,0,0), p.color(0,0,255), getDistanceFromMouse(-10 + (i*50), 250));
+			const lerpColorResult = p.lerpColor(p.color(245, 158, 66), p.color(19, 43, 148), getDistanceFromMouse(-10 + (i*50), 250));
 			p.fill(lerpColorResult)
             p.beginShape();
-                p.vertex(-10 + (i*50) - (getDistanceFromMouse(-10 + (i*50), 0)*30), 0);
+                p.vertex(xVertCalc(middleX, 0, i, "-"), 0);
                 for (let j = 1; j <= 4; j++) {
                     const vertY = ((500/4)*j) - (i*10);
-					console.log(p.dist(-20 + (i*50),vertY, p.mouseX, p.mouseY));
 					
-					p.vertex(-20 + (i*50) - (getDistanceFromMouse(-20 + (i*50), vertY)*30), vertY);
-					p.vertex(0 + (i*50) - (getDistanceFromMouse(0 + (i*50), vertY)*30), vertY);
+					p.vertex(xVertCalc(lowX, vertY, i, "-"), vertY);
+					p.vertex(xVertCalc(middleX, vertY, i, "-"), vertY);
                 }
 
                 // bottom vert 1
-                p.vertex(-20 + (i*50) - (getDistanceFromMouse(-20 + (i*50), 500)*30), 500);
+                p.vertex(xVertCalc(lowX, 500, i, "-"), 500);
 
                 // bottom vert 2
-                p.vertex(-0 + (i*50) - (getDistanceFromMouse(-0 + (i*50), 500)*30), 500);
+                p.vertex(xVertCalc(middleX, 500, i, "-"), 500);
 
                 for (let k = 4; k >= 1; k--) {
                     const vertY = ((500/4)*k - 20) - (i*10);
 					
-					p.vertex(20 + (i*50) + (getDistanceFromMouse(20 + (i*50), vertY)*30), vertY);
-					p.vertex(0 + (i*50) + (getDistanceFromMouse(0 + (i*50), vertY)*30), vertY);
+					p.vertex(xVertCalc(highX, vertY, i, "+"), vertY);
+					p.vertex(xVertCalc(middleX, vertY, i, "+"), vertY);
                 }
 
-                p.vertex(10 + (i*50) - (getDistanceFromMouse(10 + (i*50), 0)*30), 0);
+                p.vertex(xVertCalc(highX, 0, i, "-"), 0);
             p.endShape();
         }
 		p.pop();
